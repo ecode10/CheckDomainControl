f560c67f34782a14b29278d54c77bd297a53cab5
Mauricio Junior <mauricio.junior@gmail.com>
2023-11-29 14:17:42 -0600

    add project
    add project

---
+++ CheckDomainControl/Program.cs
@@ -0,0 +1,14 @@
+﻿using CheckDomain;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var check = new Main();
+
+var result = check.ViaEmail("ecode10@mauriciojunior.net");
+
+Console.WriteLine("Result email: " + result.ToString());
+
+var result2 = check.ViaName("www.mauriciojunior.net");
+
+Console.WriteLine("Result name: " + result2.ToString());
